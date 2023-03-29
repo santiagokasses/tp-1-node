93f27edf3b5bb528b8fada70a46b4f6fe088a730
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-03-29 10:31:55 -0300

    ej 2 y algo del 4

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-//const { calcularPrecio } = require("./hola")
-//console.log(calcularPrecio(100))
+const matematica = require("./src/modules/matematica")
+const file = require('fs');
 
 var nombre = "axel"
 var apellido = "navarro"
 
 console.log(`Hola ${nombre} ${apellido}`)
+
+
+console.log(matematica.PI)
---
+++ src/modules/matematica.js
@@ -0,0 +1,15 @@
+const PI = 3.14
+
+const suma = (num1, num2) => (num1 + num2)
+const resta = (num1, num2) => (num1 - num2)
+
+function multxPI(num1){
+    return num1*PI
+}
+
+module.exports = {
+    suma,
+    resta,
+    multxPI,
+    PI
+}
