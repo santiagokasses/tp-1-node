c6a9e1dde0539d602e4b1cf2a56a60419ac182e6
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-03-29 22:56:05 -0300

    ej 4

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const matematica = require("./src/modules/matematica")
-const file = require('fs');
+const fs = require('fs');
+const readline = require('readline');
+
+
+
+fs.readFile("./txt/dai.txt", "utf8", (err, data) => {
+  if (err) return console.error(err)
+  console.log(data)
+  fs.writeFile("./txt/dai2.txt", data , err =>{
+    if (err) return console.log(err)
+    console.log("El archivo fue creado correctamente")
+  })
+})
 
 var nombre = "axel"
 var apellido = "navarro"
